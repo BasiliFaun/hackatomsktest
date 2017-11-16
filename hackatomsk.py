d7c07d4b4cb4b247183e1e3b4cbe1b43daaeeb35
basilio <catred@mail2000.ru>
2017-11-16 23:13:43 +0700

    results sorting

--- hackatomsk.py
+++ hackatomsk.py
@@ -1,15 +1,19 @@
 import asyncio
 import random
-import time
 
 async def calc(x):
-    bt=time.time()
     await asyncio.sleep(random.uniform(0.0,1.0))
-    et=time.time()
-    print ("result: {}, time: {}".format(2*x, et-bt))
+    return 2*x
 
+async def asynchronous():
+    futures = [calc(i) for i in range(10)]
+    done, _ = await asyncio.wait(futures)
+    #for task in done:
+    #    print(task.result())
+    for future in sorted(done,key=lambda x: x.result()):
+        print(future.result())
 
 ioloop = asyncio.get_event_loop()
-tasks = [ioloop.create_task(calc(i)) for i in range(10)]
-ioloop.run_until_complete(asyncio.wait(tasks))
+
+ioloop.run_until_complete(asynchronous())
 ioloop.close()
